01502283085e08bed1bf4e4838129d325cbd39ca
Ruizhen Guo <ruizhen@meta.com>
2025-12-23 19:25:31 -0800

    working for first

--- cs336_systems/flash_attention.py
+++ cs336_systems/flash_attention.py
@@ -39,6 +39,7 @@ class FlashAttentionPytorch(autograd.Function):
                 K_j = k_transformed[j * FlashAttentionPytorch.b_k:(j + 1) * FlashAttentionPytorch.b_k, :]
                 V_j: Float[torch.Tensor, "b_k d_model"] = v_transformed[j * FlashAttentionPytorch.b_k:(j + 1) * FlashAttentionPytorch.b_k, :]
                 S_i_j = einsum(Q_i, K_j, "b_q d_model, b_k d_model -> b_q b_k")
+                S_i_j = S_i_j / (D ** 0.5)
                 m_i_j = torch.max(S_i_j, dim=1).values
                 m_i_j = torch.maximum(m_i_pre, m_i_j)
 
@@ -62,6 +63,8 @@ class FlashAttentionPytorch(autograd.Function):
             O_i = einsum(diag_temp, O_i_pre, "b_q b_q, b_q d_model -> b_q d_model")
             L_i = m_i_pre + torch.log(l_i_pre)
 
+            print(O_i)
+
             O_transformed[i*FlashAttentionPytorch.b_q:(i+1)*FlashAttentionPytorch.b_q, :] = O_i
             L_transformed[i*FlashAttentionPytorch.b_q:(i+1)*FlashAttentionPytorch.b_q, ] = L_i
 
@@ -69,6 +72,7 @@ class FlashAttentionPytorch(autograd.Function):
         L = rearrange(L_transformed, "(B n_q) -> B n_q", B=B, n_q=n_q)
         ctx.save_for_backward(q, k, v, O, L)
         print(f"!!!! ruizhen O shape = {O.shape},  L = {L.shape}")
+
         return O
 
 
