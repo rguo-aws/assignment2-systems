7fcbe1e12e117a12b019756dbacc2b2f6c9548da
Ruizhen Guo <ruizhen@meta.com>
2025-12-25 13:27:01 -0800

    Flash Attention Triton forward

--- tests/adapters.py
+++ tests/adapters.py
@@ -5,6 +5,7 @@ from typing import Type
 import torch
 
 from cs336_systems.flash_attention import FlashAttentionPytorch
+from cs336_systems.flash_attention_triton import FlashAttentionTriton
 
 def get_flashattention_autograd_function_pytorch() -> Type:
     """
@@ -32,7 +33,7 @@ def get_flashattention_autograd_function_triton() -> Type:
         A class object (not an instance of the class)
     """
     # For example: return MyTritonFlashAttentionAutogradFunctionClass
-    raise NotImplementedError
+    return FlashAttentionTriton
 
 
 def get_ddp_individual_parameters(module: torch.nn.Module) -> torch.nn.Module:
