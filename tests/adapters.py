88a368ca80e17080548c0b17659567c49c121661
Ruizhen Guo <ruizhen@meta.com>
2025-12-29 18:35:46 -0800

    DDPBucketed WIP@

--- tests/adapters.py
+++ tests/adapters.py
@@ -6,7 +6,7 @@ import torch
 
 from cs336_systems.flash_attention import FlashAttentionPytorch
 from cs336_systems.flash_attention_triton import FlashAttentionTriton
-from cs336_systems.dpp_overlap import DDPIndividualParameters
+from cs336_systems.dpp_overlap import DDPIndividualParameters, DDPBucketed
 
 def get_flashattention_autograd_function_pytorch() -> Type:
     """
@@ -92,7 +92,7 @@ def get_ddp_bucketed(module: torch.nn.Module, bucket_size_mb: float) -> torch.nn
     Returns:
         Instance of a DDP class.
     """
-    raise NotImplementedError
+    return DDPBucketed(module, bucket_size_mb)
 
 
 def ddp_bucketed_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
@@ -106,8 +106,7 @@ def ddp_bucketed_on_after_backward(ddp_model: torch.nn.Module, optimizer: torch.
         optimizer: torch.optim.Optimizer
             Optimizer being used with the DDP-wrapped model.
     """
-    # For example: ddp_model.finish_gradient_synchronization()
-    raise NotImplementedError
+    ddp_model.finish_gradient_synchronization()
 
 
 def ddp_bucketed_on_train_batch_start(ddp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
@@ -120,7 +119,7 @@ def ddp_bucketed_on_train_batch_start(ddp_model: torch.nn.Module, optimizer: tor
         optimizer: torch.optim.Optimizer
             Optimizer being used with the DDP-wrapped model.
     """
-    raise NotImplementedError
+    return None
 
 
 def get_sharded_optimizer(params, optimizer_cls: Type[torch.optim.Optimizer], **kwargs) -> torch.optim.Optimizer:
